eb7960fc0c51d9f9c4681e2c3be0ca4695faaa34
koreginev <evgkoregin@gmail.com>
2026-02-11 08:57:12 +0300

    First commit

---
+++ src/main/java/ru/korevg/fimas/FimasApplication.java
@@ -0,0 +1,13 @@
+package ru.korevg.fimas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FimasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FimasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/korevg/fimas/FimasApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.korevg.fimas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FimasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
